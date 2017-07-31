502a94bf27eea3336c9e6494d28d0fd5f8d85dff
Evan Moon <emoon002@gmail.com>
2017-07-31 04:15:55 -0700

    Added button to return user to top of page and added footer and copyright text

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//Scroll to the top of the page when user clicks the button
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
